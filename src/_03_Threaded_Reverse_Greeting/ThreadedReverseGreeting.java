e96697342115ad4921aa493036a47875a89e3fb0
League Student <league@iMac-25.attlocal.net>
2019-11-23 17:24:57 -0800

    werid, but almost done with 4th one. Like should move onto the 5th one soonish

--- src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
+++ src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
@@ -1,23 +1,32 @@
 package _03_Threaded_Reverse_Greeting;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
-	
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+
 	/* HINT: You will most likely need to do this with recursion */
 	public static void main(String[] args) {
-		for (int i = 0; i < 50; i++){
-			String threadNum = "Thread" + i;
-			Thread thredNum = new Thread(()->{
-				
+		for (int i = 50; i > 0; i--) {
+			int q = i;
+			Thread t = new Thread(() -> {
+				System.out.println("Hello from Thread " + q + "!");
+
 			});
-				
-			}
-		}
-		for (int i = 0; i < 50; i++) {
-			threadNum.
+			t.start();
 		}
 	}
 }
+// for (int i = 0; i < 50; i++){
+//			String threadNum = "Thread" + i;
+//			Thread  = new Thread(()->{
+//				System.out.println("Hello from Thread "+ i + "!");
+//			});
+//				
+//			}
+//		for (int i = 0; i < 50; i++) {
+//			
+//		}
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,11 +1,20 @@
 package _04_Thread_Pool;
 
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	Worker(ConcurrentLinkedQueue<Task> taskQueue) {
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+	}
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		while(taskQueue.size()>0) {
+			taskQueue.remove().perform();
+		}
 		
 	}
 
